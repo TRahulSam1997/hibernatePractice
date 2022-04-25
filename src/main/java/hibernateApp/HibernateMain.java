acd9200c243e6d504009aa3a81ff2617ea7e1f06
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-04-25 18:46:05 +1000

    mapping relations update

--- src/main/java/hibernateApp/HibernateMain.java
+++ src/main/java/hibernateApp/HibernateMain.java
@@ -10,37 +10,60 @@ import org.hibernate.service.ServiceRegistry;
 public class HibernateMain {
     public static void main(String[] args) {
         {
-//            System.out.println("Hello World!");
-            MentorName mn = new MentorName();
-            mn.setFname("Lev");
-            mn.setLname("Tolstoy");
-            mn.setMname("Nikolayevich");
+            Laptop laptop = new Laptop();
+            laptop.setLid(101);
+            laptop.setName("Macbook");
 
-            Mentor mentor = new Mentor();
-            mentor.setUserId(101);
-            mentor.setMentorName(mn);
-            mentor.setUserName("Tolstoy");
-            mentor.setProfession("Author");
+            Mentee m = new Mentee();
+            m.setName("Rahul");
+            m.setMenteeId(1);
+            m.setMarks(80);
+            m.getLaptop().add(laptop);
 
-            Configuration con = new Configuration().configure().addAnnotatedClass(Mentor.class);
-
-            ServiceRegistry reg = new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
-
-//            ServiceRegistry reg = ServiceRegistryImplementor
-
-            SessionFactory sf = con.buildSessionFactory(reg);
-
+            Configuration config = new Configuration().configure().addAnnotatedClass(Mentee.class).addAnnotatedClass(Laptop.class);
+            ServiceRegistry reg = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
+            SessionFactory sf = config.buildSessionFactory(reg);
             Session session = sf.openSession();
 
-            Transaction tx = session.beginTransaction();
+            session.beginTransaction();
 
-            session.save(mentor);
+            session.save(laptop);
+            session.save(m);
 
-//            dom = (Mentor) session.get(Mentor.class, 106);
+            session.getTransaction().commit();
 
-            tx.commit();
+////            System.out.println("Hello World!");
+//            MentorName mn = new MentorName();
+//            mn.setFname("Lev");
+//            mn.setLname("Tolstoy");
+//            mn.setMname("Nikolayevich");
+//
+//            Mentor mentor = new Mentor();
+//            mentor.setUserId(101);
+//            mentor.setMentorName(mn);
+//            mentor.setUserName("Tolstoy");
+//            mentor.setProfession("Author");
+//
+//            Configuration con = new Configuration().configure().addAnnotatedClass(Mentor.class);
+//
+//            ServiceRegistry reg = new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
+//
+////            ServiceRegistry reg = ServiceRegistryImplementor
+//
+//            SessionFactory sf = con.buildSessionFactory(reg);
+//
+//            Session session = sf.openSession();
+//
+//            Transaction tx = session.beginTransaction();
+//
+//            session.save(mentor);
+//
+////            dom = (Mentor) session.get(Mentor.class, 106);
+//
+//            tx.commit();
+//
+//            System.out.println(mentor);
 
-            System.out.println(mentor);
         }
     }
 }
---
+++ src/main/java/hibernateApp/Laptop.java
@@ -0,0 +1,28 @@
+package hibernateApp;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Laptop {
+
+    @Id
+    private int lid;
+    private String name;
+
+    public int getLid() {
+        return lid;
+    }
+
+    public void setLid(int lid) {
+        this.lid = lid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
